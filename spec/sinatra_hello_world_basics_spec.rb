ca70205f736fd8fdd577e1138f148a710049feeb
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-10-01 17:00:53 +0000

    Automatically backed up by Learn

--- spec/sinatra_hello_world_basics_spec.rb
+++ spec/sinatra_hello_world_basics_spec.rb
@@ -3,11 +3,11 @@ describe "GET '/'" do
     get '/'
   end
 
-  xit "responds with a 200 status code" do
+  it "responds with a 200 status code" do
     expect(last_response.status).to eq(200)
   end
 
-  xit "responds with 'Hello, World!'" do
+  it "responds with 'Hello, World!'" do
     expect(last_response.body).to include("Hello, World!")
   end
 end
